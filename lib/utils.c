fe2ad80543560884f4b6f907afd3bdfedd09d4b8
Matheus E. Muller <hello@memuller.com>
2017-05-29 23:36:21 -0300

    Adds a method to sobel-transform a single pixel

--- lib/utils.c
+++ lib/utils.c
@@ -52,3 +52,33 @@ ppm_image addMargins(ppm_image original){
   }
   return result;
 }
+
+void transformPixelSobel(ppm_image original, unsigned int x, unsigned int y, ppm_image new){
+
+  const char sobel_x[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
+  const char sobel_y[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
+
+  unsigned char fa = GET_PIXEL(original, x-1,y-1)[0];
+  unsigned char fb = GET_PIXEL(original, x,y-1)[0];
+  unsigned char fc = GET_PIXEL(original, x+1,y-1)[0];
+  unsigned char fd = GET_PIXEL(original, x-1,y)[0];
+  unsigned char fe = GET_PIXEL(original, x,y)[0];
+  unsigned char ff = GET_PIXEL(original, x+1,y)[0];
+  unsigned char fg = GET_PIXEL(original, x-1,y+1)[0];
+  unsigned char fh = GET_PIXEL(original, x,y+1)[0];
+  unsigned char fi = GET_PIXEL(original, x+1,y+1)[0];
+
+  short pixel_x = (sobel_x[0][0] * fa ) + (sobel_x[0][1] * fb ) + (sobel_x[0][2] * fc) + (sobel_x[1][0] * fd) + (sobel_x[1][1] * fe) + (sobel_x[1][2] * ff) + (sobel_x[2][0] * fg) + (sobel_x[2][1] * fh) + (sobel_x[2][2] * fi);
+
+  short pixel_y = (sobel_y[0][0] * fa ) + (sobel_y[0][1] * fb ) + (sobel_y[0][2] * fc) + (sobel_y[1][0] * fd) + (sobel_y[1][1] * fe) + (sobel_y[1][2] * ff) + (sobel_y[2][0] * fg) + (sobel_y[2][1] * fh) + (sobel_y[2][2] * fi);
+
+  short val = abs(pixel_x) + abs(pixel_y);
+  unsigned char char_val = (255-(unsigned char)(val));
+
+  if(char_val > 200)
+      char_val = 255;
+  else if(char_val < 100)
+      char_val = 0;
+
+  put_pixel_unsafe(new, x, y, char_val, char_val, char_val);
+}
