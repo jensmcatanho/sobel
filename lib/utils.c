1bff69cf6a935753043481ceb0ac1b1f82aa3377
Matheus E. Muller <hello@memuller.com>
2017-05-30 15:41:46 -0300

    Adds a function for reading PPM to shared memory

--- lib/utils.c
+++ lib/utils.c
@@ -128,3 +128,47 @@ ppm_image allocSharedImage(unsigned int width, unsigned long height){
   image->max_color = 255;
   return image;
 }
+
+/**
+ * like Nery's get_ppm, but reads to a shared memory block.
+ * reads the given file pointer as a PPM and stores it in memory as ppm_image
+ * @param  pf : the file. read permissions needed
+ * @return  ppm_image : the image
+ */
+ppm_image getPpmShared(FILE *pf)
+{
+        char buf[PPMREADBUFLEN], *t;
+        ppm_image img;
+        unsigned int w, h, d;
+        int r;
+
+        if (pf == NULL) return NULL;
+        t = fgets(buf, PPMREADBUFLEN, pf);
+        /* the code fails if the white space following "P6" is not '\n' */
+        if ( (t == NULL) || ( strncmp(buf, "P6\n", 3) != 0 ) ) return NULL;
+        do
+        { /* Px formats can have # comments after first line */
+           t = fgets(buf, PPMREADBUFLEN, pf);
+           if ( t == NULL ) return NULL;
+        } while ( strncmp(buf, "#", 1) == 0 );
+        r = sscanf(buf, "%u %u", &w, &h);
+        if ( r < 2 ) return NULL;
+
+        r = fscanf(pf, "%u", &d);
+        if ( (r < 1) || ( d != 255 ) ) return NULL;
+        fseek(pf, 1, SEEK_CUR); /* skip one byte, should be whitespace */
+
+        img = allocSharedImage(w, h);
+        if ( img != NULL )
+        {
+            img->high = 0;
+            size_t rd = fread(img->buf, sizeof(pixel), w*h, pf);
+            if ( rd < w*h )
+            {
+               free_img(img);
+               return NULL;
+            }
+            return img;
+        }
+        return NULL;
+}
