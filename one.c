710dfc3ba5b4ec557750a9351c79f2b3a71ef2e5
Matheus E. Muller <hello@memuller.com>
2017-05-30 02:12:29 -0300

    Sobelizes w/ process =D

--- one.c
+++ one.c
@@ -5,39 +5,61 @@
 #include <sys/wait.h>
 #include <string.h>
 
+void sobelImageSlice(ppm_image original, unsigned int start, unsigned int end, ppm_image destination){
+  for(unsigned int i = start; i < end; i++){
+    for(unsigned int j = 1; j < destination->height -2; j++){
+      transformPixelSobel(original, i, j, destination);
+    }
+  }
+  char file[20];
+  // sprintf(file, "debug%i.ppm", start);
+  // saveImage(destination, file);
+}
+
 int main(int numArgs, char **args){
 
-  // char* filePath = "./data/rio_map.ppm";
-  //
-  // FILE* originalImageFile = fopen(filePath, "rb");
-  // if(!originalImageFile) fail("Could not read original image file");
-  //
-  // ppm_image originalImage = get_ppm(originalImageFile);
-  // if(!originalImage) fail("Could not alloc PPM for the original image");
-  //
-  // ppm_image wMargins = addMargins(originalImage);
-  //
-  // to_greyscale(wMargins);
-  // ppm_image result = alloc_img(wMargins->width, wMargins->height);
-  // fill_img(result, 0,0,0);
-  //
-  // for(unsigned int i = 1; i < wMargins->width -2; i++){
-  //   for(unsigned int j = 1; j < wMargins->height -2; j++){
-  //     transformPixelSobel(wMargins, i, j, result);
-  //   }
-  // }
-  //
-  // quickSaveImage(result);
-  //
-  unsigned short nProcesses = 4;
-  unsigned short waiting = nProcesses;
-  pid_t pids[nProcesses];
+  char* filePath = "./data/escola_artes_visuais.ppm";
 
-  for(unsigned short i = 0; i < nProcesses; i++){
-    pids[i] = fork();
+  // reads source image file
+  FILE* originalImageFile = fopen(filePath, "rb");
+    if(!originalImageFile) fail("Could not read original image file");
+
+  // loads source image
+  ppm_image originalImage = get_ppm(originalImageFile);
+    if(!originalImage) fail("Could not alloc PPM for the original image");
+
+  // adds margin to image
+  ppm_image wMargins = addMargins(originalImage);
+  free_img(originalImage); // frees memory from original image
+  to_greyscale(wMargins); // converts to grayscale
+
+  // allocates resulting image
+  ppm_image result = allocSharedImage(wMargins->width, wMargins->height);
+  fill_img(result, 0,0,0); // fills it with black pixels
+
+  // Process control
+  unsigned short numProcesses = 4;
+  unsigned short waiting = numProcesses; //the number of unfinished processes
+  pid_t pids[numProcesses]; //array which will have the PIDs of child processes
+
+  //section of the image each process will work on
+  unsigned int sliceSize = wMargins->width / numProcesses;
+  unsigned int remainderSlice = wMargins->width % numProcesses; //if the division was uneven, stores the remainder so we can add it later to the last slice
+
+  // forks [numProcesses] processes.
+  for(unsigned short i = 0; i < numProcesses; i++){
+    pids[i] = fork(); //adds the forked process PID to pids[]
     if(pids[i] == -1) fail("Failed at forking process");
+
     if(pids[i] == 0){
-      printf("%i \n", i);
+      unsigned int start = (i * sliceSize);
+      unsigned int end = start + sliceSize;
+      // if at the first slice, adds +1 to starting collumn
+      if(start == 0) start++;
+      // if at the last slice, adds the remainder collumns in case the slice division was uneven
+      if(i >= numProcesses-1) end += remainderSlice;
+      printf("process %i handling collumns %i-%i\n", getpid(), start, end);
+      sobelImageSlice(wMargins, start, end, result);
       exit(0);
     }
   }
@@ -49,6 +71,7 @@ int main(int numArgs, char **args){
     waiting--;
   }
 
+  quickSaveImage(result);
 
   return 0;
 }
